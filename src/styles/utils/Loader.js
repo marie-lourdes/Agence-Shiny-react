d0be4fe5d5cf2a1c3a02ca638151ce251ebd1ce3
marie-lourdes <marielourdesleperlier64@gmail.com>
2022-11-24 16:16:14 +0100

    style: white space loader.js

--- src/styles/utils/Loader.js
+++ src/styles/utils/Loader.js
@@ -4,88 +4,61 @@ import { keyframes } from 'styled-components'
 
 const opacity = keyframes`
     from {
-       opacity: 0.5;
-       
+       opacity: 0.5;      
     }
  
     to {
-   opacity:1;
-   
+      opacity:1; 
     }
 `
 
 const rotateSpinner = keyframes`
     from {
         transform: rotate(0deg);
-        opacity: 0;
-       
+        opacity: 0;     
     }
  
     to {
         transform: rotate(45deg);
-    opacity: 1;
-    
-  
+        opacity: 1;    
     }
 `
 
-
 export const Spinner = styled.div`  
 `
-
-
-export const Loader = styled.div`
-
-    
-    padding: 16px;
-  
+export const Loader = styled.div` 
+    padding: 15px;
     animation: ${opacity} 4000ms -2000ms infinite linear;
-    position: relative;
-   
+    position: relative; 
     height:0px;
     width: 0px;
    ${Spinner} {
- 
-        position: absolute;
-        
-        animation: ${rotateSpinner} 1000ms infinite ease-out;
-     
+        position: absolute;       
+        animation: ${rotateSpinner} 1000ms infinite ease-out;  
         border-radius: 50%;
         top: 0;
         left:0;
         right:0;
         bottom:0;
 
-        
         &.spinner1 {
             border: 8px solid #8932BA ;
-            border-top-color ${colors.primary};
-           
-           
+            border-top-color ${colors.primary};      
         }
 
-        &.spinner2 {
-           
-            border-right: 8px solid ${colors.primary};  
-          
-            animation-delay: 250ms;
-            
+        &.spinner2 {         
+            border-right: 8px solid ${colors.primary};       
+            animation-delay: 250ms;      
         }
 
         &.spinner3 {
             border-bottom: 8px solid ${colors.primary};  
-        
-            animation-delay: 500ms;
-         
+            animation-delay: 500ms;     
         }
 
         &.spinner4 {
-            border-left: 8px solid ${colors.primary};  
-     
-            animation-delay: 750ms;
-           
+            border-left: 8px solid ${colors.primary};   
+            animation-delay: 750ms;         
         }
-    }
-       
-
+    }      
 `
