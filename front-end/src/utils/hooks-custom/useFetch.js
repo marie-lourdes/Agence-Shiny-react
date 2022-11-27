b75a00c5082e29818e05f9a3d76600d4a067862c
marie-lourdes <marielourdesleperlier64@gmail.com>
2022-11-27 19:12:50 +0100

    commit

--- front-end/src/utils/hooks-custom/useFetch.js
+++ front-end/src/utils/hooks-custom/useFetch.js
@@ -12,7 +12,13 @@ export function useFetch(url) {
 
     useEffect(() => {
 
+        // arrete la fonction useEffect
         if (!url) return
+
+        // delimite le debut du chargement des donnés ce qui evite les moment de blanc qui affichera a la place un loader dans le render du composant
+        // avant la mise a jour du state avec les données de l api
+        setIsLoading(true)
+
         {/* avec l enchainement des promesses then qui recuperes les promesses resolu , non resolu, pending ci-dessous la requete ne fonctionné pas avec le hook useFetch 
  et tout partculierement le useEffect qui ne tolere pas les fonctions asynchrone mais si on apelle directement la fonction asynchrone a l interieur ca fonctionne 
 Utiliser une async fonction fait retourner à la fonction de retour une Promesse au lieu d'une fonction de nettoyage.
@@ -24,26 +30,43 @@ Cette fonction va également retourner une Promesse, peu importe si l'on a expli
  On apelle directement la fonction async  sans callBack en parametre de la fonction asynchrone
 */}
         async function fetchData() {
+            try {
 
-            const response = await fetch(url)
-            const data = await response.json()
+                const response = await fetch(url)
 
-            setData(data)
-            setIsLoading(false)
+                const data = await response.json()
+
+                setData(data)
+
+            } catch (err) {
+                console.log(err)
+
+                // retourne un erreur dans le state qui actvera l affichage d un message d erreur dans le dom dans survey.jsx
+                // a la recuperation du state isError dans survey.jsx
+                setError(true)
+
+            } finally {
+                setIsLoading(false)
+            }
         }
-
-        setIsLoading(true)
 
         fetchData()
 
     }, [url])
+    // le useEffect se declenche a chaque changement d url avec le tableau de dependance
 
+    // le hook useFetch retourne un objet les states
     return { isLoading, datas, isError }
 
 }
 
 
-/*    useEffect(() => {
+/*  
+export function useFetch(url) {
+    const [isLoading, setIsLoading] = useState(false)// par defaut on met le state is loading a false 
+    const [datas, setData] = useState({})
+    const [isError, setError] = useState(false) 
+ useEffect(() => {
         if (!url) return
         // arrete la fonction useEffect
         // delimite le debut du chargement des donnés ce qui evite les moment de blanc qui affichera a la place un loader dans le render du composant
@@ -58,10 +81,6 @@ Cette fonction va également retourner une Promesse, peu importe si l'on a expli
                 console.log("data surveydata", data)
 
                 setData(data)
-
-
-
-
 
                 setIsLoading(false)
             })
