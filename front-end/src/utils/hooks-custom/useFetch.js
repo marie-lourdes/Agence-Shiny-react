995294faeee99164017dd21eee28989a62c86321
marie-lourdes <marielourdesleperlier64@gmail.com>
2022-11-28 10:12:05 +0100

    comments

--- front-end/src/utils/hooks-custom/useFetch.js
+++ front-end/src/utils/hooks-custom/useFetch.js
@@ -1,22 +1,46 @@
 import { useState, useEffect } from 'react'
 
+/* arrete la fonction useEffect
+if (!url) return
 
+// delimite le debut du chargement des donnés ce qui evite les moment de blanc qui affichera a la place un loader dans le render du composant
+// avant la mise a jour du state avec les données de l api
+setIsLoading(true)
+
+/* avec l enchainement des promesses then qui recuperes les promesses resolu , non resolu, pending ci-dessous la requete ne fonctionné pas avec le hook useFetch 
+et tout partculierement le useEffect qui ne tolere pas les fonctions asynchrone mais si on apelle directement la fonction asynchrone a l interieur ca fonctionne 
+Utiliser une async fonction fait retourner à la fonction de retour une Promesse au lieu d'une fonction de nettoyage.
+React n'attends pas une Promesse mais une fonction.
+Function aync :
+Une fonction qui permet d'utiliser des instructions asynchrone avec le mot clé await, ce qui bloquera l'execution du code tant que la Promesse après laquelle le mot cléawaitse trouve n'aura pas résolu…
+Cette fonction va également retourner une Promesse, peu importe si l'on a explicitement retourné quelque chose ou non( lobjet vide de useState). Dans le cas où l'on retourne une donnée,
+elle sera enveloppée dans le contenu de resolution de la Promese que la fonction va créer et retourner automatiquement.
+On apelle directement la fonction async  sans callBack en parametre de la fonction asynchrone*/
 export function useFetch(url) {
     const [data, setData] = useState({})
     const [isLoading, setLoading] = useState(true)
     const [error, setError] = useState(false)
+    // apres le premier render (uniquement) du composant dont le state est vide,
+    //useEffet execute la requête avec fetch qui met a jour le state avec les données requêté sur l api,
+    //le composant est re-render avce le state mis a jour mais useEffect n execute plus la requete apres,
+    // malgré le re-render de mise a jour du state grace  au tableau vide
+
 
     useEffect(() => {
         if (!url) return
         setLoading(true)
         async function fetchData() {
             try {
+                //Si on passe une promesse à une expression await,
+                // celle-ci attendra jusqu'à la résolution de la promesse et renverra la valeur de résolution.
                 const response = await fetch(url)
                 const data = await response.json()
                 setData(data)
             } catch (err) {
                 console.log(err)
                 setError(true)
+                // retourne un erreur dans le state qui actvera l affichage d un message d erreur dans le dom dans survey.jsx
+                // a la recuperation du state isError dans survey.jsx
             } finally {
                 setLoading(false)
             }
@@ -24,6 +48,11 @@ export function useFetch(url) {
         fetchData()
     }, [url])
     return { isLoading, data, error }
+    // le useEffect se declenche a chaque changement d url avec le tableau de dependance
+
+    // le hook useFetch retourne un objet les states
+
+
 }
 
 /*export function useFetch(url) {
